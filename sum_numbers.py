8b187d47ac2a28fbc2c939cb2354d678b2e60ae1
Ruchi-Bhanot <ruchi_bhanot@udacity.com>
2018-07-25 22:48:20 -0700

    initial set up

---
+++ sum_numbers.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+This program adds two numbers
+"""
+num1 = '334.2'
+num2 = '791.1'
+
+# Add the two numbers
+sum = num1 + num2
+
+# print the sum
+print "The sum of num1 {} and num2 {} is sum {}."
+
+
